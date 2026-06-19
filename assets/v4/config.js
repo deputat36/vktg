5aaa80f15d1f44992b0fb1ed920f6ce5d8945f99
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-06-19 12:29:22 +0300

    Relax Supabase request timeouts for unstable networks

---
+++ assets/v4/config.js
@@ -0,0 +1,12 @@
+export const V4_CONFIG = Object.freeze({
+  supabaseUrl: 'https://ofewxuqfjhamgerwzull.supabase.co',
+  supabasePublishableKey: 'sb_publishable_ZiX8_Mnf0dY6S__tKO2A4A_uD94G2cs',
+  authStorageKey: 'leader_crm_v4_session',
+  timeouts: Object.freeze({
+    sessionMs: 12000,
+    loginMs: 22000,
+    logoutMs: 10000,
+    profileMs: 8000,
+    requestMs: 25000
+  })
+});
