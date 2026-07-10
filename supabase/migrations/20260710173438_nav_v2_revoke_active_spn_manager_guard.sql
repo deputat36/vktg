da6e7fe2c81cfe8c5e285fcbce267e201f306341
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-07-10 20:35:45 +0300

    Restrict active SPN manager trigger function grants

---
+++ supabase/migrations/20260710173438_nav_v2_revoke_active_spn_manager_guard.sql
@@ -0,0 +1,3 @@
+revoke execute on function public.nav_v2_guard_active_spn_manager() from public;
+revoke execute on function public.nav_v2_guard_active_spn_manager() from anon;
+revoke execute on function public.nav_v2_guard_active_spn_manager() from authenticated;
