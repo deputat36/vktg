86cea11f663e242098b35b706e33792cf11abbf0
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-07-15 17:24:32 +0300

    Define hidden help text for working fields

--- assets/js/nav-v2/form-association-model-v2.js
+++ assets/js/nav-v2/form-association-model-v2.js
@@ -2,43 +2,50 @@ const FIELD_POLICIES = Object.freeze({
   dealSearch: Object.freeze({
     id: 'dealSearch',
     name: 'Поиск сделок',
-    helpIds: Object.freeze(['dealFiltersHelp']),
+    helpIds: Object.freeze(['dealSearchHelp']),
+    helpText: 'Ищет по адресу, объекту, клиенту, СПН, статусу или идентификатору сделки.',
     statusId: ''
   }),
   dealFilter: Object.freeze({
     id: 'dealFilter',
     name: 'Режим списка сделок',
-    helpIds: Object.freeze(['dealFiltersHelp']),
+    helpIds: Object.freeze(['dealFilterHelp']),
+    helpText: 'Ограничивает рабочую очередь выбранным режимом, не изменяя данные сделок.',
     statusId: ''
   }),
   dealStatus: Object.freeze({
     id: 'dealStatus',
     name: 'Текущий статус сделки',
     helpIds: Object.freeze(['dealStatusHelp']),
+    helpText: 'Выберите следующий этап сделки и сохраните изменение.',
     statusId: 'pageStatus'
   }),
   newComment: Object.freeze({
     id: 'newComment',
     name: 'Новый комментарий',
     helpIds: Object.freeze(['newCommentHelp']),
+    helpText: 'Комментарий увидят участники команды сделки.',
     statusId: 'pageStatus'
   }),
   spnReworkCompletionText: Object.freeze({
     id: 'spnReworkCompletionText',
     name: 'Что именно исправлено',
     helpIds: Object.freeze(['spnReworkCompletionHelp']),
+    helpText: 'Перечислите сохранённые изменения. Этот комментарий увидят юрист и менеджер.',
     statusId: 'spnReworkStatusV2'
   }),
   spnReworkReturnReason: Object.freeze({
     id: 'spnReworkReturnReason',
     name: 'Главная причина или другое замечание',
     helpIds: Object.freeze(['spnReworkReturnHelp']),
+    helpText: 'Опишите конкретную причину, если подходящего замечания нет в списке.',
     statusId: 'spnReworkStatusV2'
   }),
   lawyerDocumentNoteV2: Object.freeze({
     id: 'lawyerDocumentNoteV2',
     name: 'Комментарий к действию по документу',
     helpIds: Object.freeze(['lawyerDocumentNoteHelpV2']),
+    helpText: 'Для проблемы укажите конкретную причину. Для остальных действий комментарий необязателен.',
     statusId: 'lawyerDocumentStatusV2'
   })
 });
@@ -83,7 +90,8 @@ export function fieldAssociationContract() {
     globalStatusNotAttachedByDefault: true,
     unknownFieldsUntouched: true,
     duplicateDescriptionIdsForbidden: true,
-    layoutMutationAllowed: false,
+    hiddenHelperInsertionAllowed: true,
+    visibleLayoutMutationAllowed: false,
     storageAllowed: false,
     networkAllowed: false
   });
